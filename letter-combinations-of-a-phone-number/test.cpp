2e20d4daf058e1f6248271412fb5e27f5d6dccdf
ArcSung <ArcLaviz@gmail.com>
2019-06-07 13:29:43 +0800

    update letter-combinations-of-a-phone-number

---
+++ letter-combinations-of-a-phone-number/test.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream> 
+#include <vector>
+#include <set>
+#include <algorithm>    // std::sort
+using namespace std; 
+
+int main(void){
+	string digits = "28";
+        vector <string> puttons({"", "abc", "def", "ghi", "jkl",     
+                                "mno", "pqrs", "tuv", "wxyz"});
+    
+        vector<string> ret;
+        
+        //if (digits.size() < 1)
+        //    return ret;
+        
+        ret.push_back("");
+        
+        for(int i = 0; i < digits.size(); i ++){
+            int num = digits[i] - '0';
+            if(num < 1 || num > 9)
+                continue;
+            string putton = puttons[num - 1];
+            
+            if(putton.size() < 3)
+                continue;
+            
+            vector<string> temp_res;
+            
+            for (int j = 0; j < putton.size(); ++j)
+            {
+                for (int k = 0; k<ret.size(); ++k)
+                {
+                    temp_res.push_back(ret[k] + putton[j]);
+                }
+
+            }
+            ret.swap(temp_res);
+
+        }
+	return true;
+}
