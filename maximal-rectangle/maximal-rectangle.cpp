771ef6768ba1e3487b50914f7ba3d7cbcca06fe6
ArcSung <ArcLaviz@gmail.com>
2019-07-10 21:18:24 +0800

    add maximal-rectangle

---
+++ maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,53 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<climits>
+#include<stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int res = 0;
+        vector<int> height;
+        for (int i = 0; i < matrix.size(); ++i) {
+            height.resize(matrix[i].size());
+            for (int j = 0; j < matrix[i].size(); ++j) {
+                height[j] = matrix[i][j] == '0' ? 0 : (1 + height[j]);
+            }
+            res = max(res, largestRectangleArea(height));
+        }
+        return res;
+    }
+    int largestRectangleArea(vector<int>& height) {
+        int res = 0;
+        stack<int> s;
+        height.push_back(0);
+        for (int i = 0; i < height.size(); ++i) {
+            if (s.empty() || height[s.top()] <= height[i]) s.push(i);
+            else {
+                int tmp = s.top(); s.pop();
+                res = max(res, height[tmp] * (s.empty() ? i : (i - s.top() - 1)));
+                --i;
+            }
+        }
+        return res;
+    }
+};
+
+
+int main(void){
+    Solution sl;
+    
+    vector<vector<char>> matrix = {{'1','0','1','0','0'},
+                                   {'1','0','1','1','1'},
+                                   {'1','1','1','1','1'},
+                                   {'1','0','0','1','0'}};
+
+    int ret = sl.maximalRectangle(matrix);
+
+    printf("ret:%d\n", ret);
+}
