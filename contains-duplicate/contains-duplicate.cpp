839bede40718155ff1a8fe9b9039958c09d52e7f
ArcSung <ArcLaviz@gmail.com>
2019-09-29 18:26:16 +0800

    add contains-duplicate

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {        
+        unordered_map<int, int> m;
+
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(m.count(nums[i])){
+                return true;
+            }
+            else
+                m[nums[i]]++;
+        }
+        
+        return false;
+    }
+};
